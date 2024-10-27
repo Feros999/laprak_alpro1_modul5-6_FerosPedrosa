9ca1c7aa122a2289fd21f55ac12e13650b274bc2
Feros999 <ferospedrosa@gmail.com>
2024-10-27 21:03:02 +0700

    pemangkatan.go

--- pemangkatan.go
+++ pemangkatan.go
@@ -9,14 +9,9 @@ func main() {
 	fmt.Print("Masukkan bilangan pangkat : ")
 	fmt.Scan(&pangkat)
 
-	// Inisialisasi hasil dengan 1 (karena bilangan apapun dipangkatkan 0 hasilnya 1)
 	result = 1
-
-	// Menghitung base^exponent menggunakan perkalian berulang
 	for i := 1; i <= pangkat; i++ {
 		result *= dasar
 	}
-
-	// Mencetak hasil pemangkatan
 	fmt.Printf("Hasil dari %d pangkat %d adalah: %d\n", dasar, pangkat, result)
 }
