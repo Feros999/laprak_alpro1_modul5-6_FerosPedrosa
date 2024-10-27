11a0c4a39c8e305849ed117575699571365571e3
Feros999 <ferospedrosa@gmail.com>
2024-10-27 09:00:20 +0700

    kumpulanbil.go

--- kumpulanbil.go
+++ kumpulanbil.go
@@ -7,11 +7,8 @@ func main() {
 	fmt.Print("Masukkan bilangan bulat positif: ")
 	fmt.Scan(&n)
 
-	// Menjumlahkan bilangan dari 1 hingga n
 	for i := 1; i <= n; i++ {
 		total += i
 	}
-
-	// Mencetak hasil penjumlahan
 	fmt.Println("Hasil penjumlahan dari 1 sampai", n, "adalah", total)
 }
