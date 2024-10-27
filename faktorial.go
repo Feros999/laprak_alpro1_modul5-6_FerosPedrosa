ae96656cbed7ea7771bb593854d2b64ee2331d39
Feros999 <ferospedrosa@gmail.com>
2024-10-27 08:58:17 +0700

    faktorial.go

--- faktorial.go
+++ faktorial.go
@@ -7,14 +7,9 @@ func main() {
 	fmt.Print("Masukkan bilangan bulat non-negatif: ")
 	fmt.Scan(&n)
 
-	// Inisialisasi hasil faktorial dengan 1 (karena faktorial 0 = 1)
 	result = 1
-
-	// Menghitung faktorial menggunakan perulangan
 	for i := 1; i <= n; i++ {
 		result *= i
 	}
-
-	// Mencetak hasil faktorial
 	fmt.Printf("Faktorial dari %d adalah: %d\n", n, result)
 }
